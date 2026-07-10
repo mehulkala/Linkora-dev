8b2f21ced109b22211a9243b9abb6db69e1276d6
mehulkala <mehulkala22@gmail.com>
2026-07-11 00:54:52 +0530

    typo fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ app.use("/api/auth", authRoutes);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (_, res)=>{
-        res.sendFile(path.join(__dirname + "../frontend/dist/index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     })
 }
 
